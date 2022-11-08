0fa596f554c211667f828977d362a4fc62c57488
Amirhamja422 <amirhamja422@gmail.com>
2022-11-09 01:44:50 +0600

    magic method

--- index.php
+++ index.php
@@ -23,3 +23,53 @@ $amir->display();
 //for static variable
 person::display();
 ?>
+
+
+
+<?php
+
+// magic method 
+
+
+//get ($property)
+//set ($property, $value)   // all are magic method
+//call($method, $arg_array) //undefined method
+
+class Student{
+	public $name;
+	public function describe(){
+     echo "I am a Student";
+	}
+	public function __get($pm){
+		echo "$pm does not match";
+
+
+	}
+
+
+public function __set($pm,$value){
+	echo "we set $pm->$value";
+}
+
+public function __call($pm,$value){
+	echo 'There is no '.$pm.' method and argument '.implode(',',$value);
+}
+
+}
+
+
+
+
+
+$st = new Student();
+$st->describe();
+$st->Amir; // thise is undefined property 
+$st->age ="12";
+$st->DEscribeIt('2','7','10');
+
+
+
+
+
+
+ ?>
