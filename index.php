065550661ce5d88c7fd7e1137e2d68eb469862d0
Amirhamja422 <amirhamja422@gmail.com>
2022-11-14 02:04:23 +0600

    oop

--- index.php
+++ index.php
@@ -1,129 +1,4 @@
 
-<!-- ///////////////////////////////////two number add ,sub ,div, mult using oop /////////////////////////////////// -->
-
-
-<!-- ///////////////////////////////////two number add ,sub ,div, mult using oop /////////////////////////////////// -->
-<!-- 
-<?php include "inc/function.php";?>
-   <form action="" method="post">
-    <table>
-      <tr>
-       <td>Enter the first number</td>
-       <td><input type="number" name="num1"></td>
-       <td>Enter the Secound number</td>
-       <td><input type="number" name="num2"></td>
-       </tr>
-       <tr>
-        <td><input type="submit" name="calculation" value="calculation"></td>
-       </tr>
-    </table>  
-   </form>
-
-  <?php
-    if(isset($_POST['calculation'])){
-      $numOne = $_POST['num1'];  
-      $numTwo =$_POST['num2'];
-
-      if(empty($numOne) or empty($numTwo)){
-        echo "<span style='color:red;'>Field must not be empty</span>";
-
-      }
-
-      $cal = new calculations();
-      $cal->twonumberAdd($numOne,$numTwo);
-      $cal->twonumberSub($numOne,$numTwo);
-      $cal->twonumberMult($numOne,$numTwo);
-      $cal->twonumberDiv($numOne,$numTwo);
-
-    }
-
-  ?>
- -->
-
-
-<!-- <?php include "inc/function.php";?>
-   <form action="" method="post">
-    <table>
-    	<tr>
-    	 <td>Enter the first number</td>
-    	 <td><input type="number" name="num1"></td>
-    	 <td>Enter the Secound number</td>
-    	 <td><input type="number" name="num2"></td>
-       </tr>
-       <tr>
-       	<td><input type="submit" name="calculation" value="calculation"></td>
-       </tr>
-    </table>	
-   </form>
-
-	<?php
-	  if(isset($_POST['calculation'])){
-	  	$numOne = $_POST['num1'];  
-	    $numTwo =$_POST['num2'];
-
-	    if(empty($numOne) or empty($numTwo)){
-	    	echo "<span style='color:red;'>Field must not be empty</span>";
-
-	    }
-
-	    $cal = new calculations();
-	    $cal->twonumberAdd($numOne,$numTwo);
-	    $cal->twonumberSub($numOne,$numTwo);
-	    $cal->twonumberMult($numOne,$numTwo);
-	    $cal->twonumberDiv($numOne,$numTwo);
-
-	  }
-
-	?>
- -->
-
-
-<?php
-//consttruct function
-// class personOne{
-//    public $names ='amirrr';
-//    public function __construct(){
-//     echo $this->names;
-//    }
-// }
-//  $personTwo = new personOne();
-?>
-
-
-
-
-
-
-<?php
-//consttruct function
-// class personOne{
-//    public function __construct($names){
-//     echo $this->names = $names;
-//    }
-// }
-//  $personTwo = new personOne('tesruser');
-?>
-
-
-
-
-
-
-
-<?php
-//only using function
-// class personOne{
-//    public function testName($names){
-//    echo $this->name =$names; //ei vbe likhle run korbe
-//    echo $names; // ei vabe likhle run korbe
-// }
-
-// }
-// $personTwo = new personOne();
-// $personTwo->testName('tesname');
-
-?>
-
 
 
 
@@ -169,18 +44,76 @@
 
 
 <?php
+
+
 //only using function
-class personOne{
-   public $names;
-   public $age;
-   public function testName($test,$age){
-   	echo $this->nameee = $test;
-   	echo "<br>";
-   	echo $this->age = $age;
+// class personOne{
+//    public $names;
+//    public $age;
+//    public function testName($test,$age){
+//    	echo $this->nameee = $test;
+//    	echo "<br>";
+//    	echo $this->age = $age;
+// }
+
+// }
+// // $personTwo = new personOne('yhh','efrgt'); eta only construct er khetrea kaj kore otherwise perameter kaj korea nah
+// $personTwo = new personOne();
+// $personTwo->testName('ere','ewfe');
+
+?>
+
+
+
+<?php
+// Bangla Object Oriented PHP Part-19 (PHP Object Iteration)
+
+
+// class Person{
+//   public $name ="amirkhan";
+//   public $age ="26";
+//   public $skil ="programmer";
+//   private $email ="amir@gmail.com";
+//   protected $password ="1234567890";
+// }
+
+// $class = new Person;
+// foreach ($class as $key => $value) {
+//   echo "<pre>"; 
+//   echo "$key => $value";
+//   echo "</pre>";
+//   # code...
+// }
+
+
+?>
+
+
+<?php
+
+
+class Person{
+  public $name ="amirkhan";
+  public $age ="26";
+  public $skil ="programmer";
+  private $email ="amir@gmail.com";
+  protected $password ="1234567890";
+
+function testName(){
+  foreach ($this as $key => $value) {
+  echo "<pre>"; 
+  echo "$key => $value";
+  echo "</pre>";
+  # code...
+}
 }
 
-}
-$personTwo = new personOne('yhh','efrgt');
-$personTwo->testName('ere','ewfe');
+ }
+
+
+
+$class = new Person;
+$class->testName();
+
 
 ?>
