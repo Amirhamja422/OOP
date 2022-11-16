0f737c90a4ce76177c1421a67e7dba8e629ab4a5
Amirhamja422 <amirhamja422@gmail.com>
2022-11-16 07:46:42 +0600

    clon obj

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php
+
+//constructor is an magic method  If you create a __construct() function, PHP will automatically call this function when you create an object from a class.
+
+//Notice that the construct function starts with two underscores (__)!
+
+public class person{
+
+public $name;
+public $age;
+
+
+public function __construct(){
+    
+}
+
+
+
+
+
+}
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -92,28 +92,156 @@
 <?php
 
 
-class Person{
-  public $name ="amirkhan";
-  public $age ="26";
-  public $skil ="programmer";
-  private $email ="amir@gmail.com";
-  protected $password ="1234567890";
+// class Person{
+//   public $name ="amirkhan";
+//   public $age ="26";
+//   public $skil ="programmer";
+//   private $email ="amir@gmail.com";
+//   protected $password ="1234567890";
 
-function testName(){
-  foreach ($this as $key => $value) {
-  echo "<pre>"; 
-  echo "$key => $value";
-  echo "</pre>";
-  # code...
+// function testName(){
+//   foreach ($this as $key => $value) {
+//   echo "<pre>"; 
+//   echo "$key => $value";
+//   echo "</pre>";
+//   # code...
+// }
+// }
+
+//  }
+
+
+
+// $class = new Person;
+// $class->testName();
+
+
+?>
+
+
+<?php 
+// kichu method independently use kora jay onno class e
+// abstratc class directly obj create korea kaj kortea pari nah
+//abstartc class ke subclass er extend korea subclass er obj crete korea use kortea pari
+//interface er khetre onek gulo interface meanse class ke implement korea kaj kora jay
+// trait er khetre interffacce er motoy onek gulo trait nea kaj kora jay
+
+// trait java{
+//   public function javaCoder(){
+//      return "i love java";
+//   }
+// }
+// trait php{
+//   public function phpCoder(){
+//      return "i love java";
+//   }
+// }
+
+
+// class coderOne{
+//   use java,php;
+// }
+
+
+
+// $obj1 = new coderOne;
+// echo $obj1->javaCoder();
+// echo $obj1->phpCoder();
+
+?>
+
+
+<?php 
+// kichu method independently use kora jay onno class e
+// abstratc class directly obj create korea kaj kortea pari nah
+//abstartc class ke subclass er extend korea subclass er obj crete korea use kortea pari
+//interface er khetre onek gulo interface meanse class ke implement korea kaj kora jay
+// trait er khetre interffacce er motoy onek gulo trait nea kaj kora jay
+
+//trait method  ke prority dei nah jodi class er vtr method thake  ota ke pririty dei
+
+// trait java{
+//   public function javaCoder(){
+//      return "i love java</br>";
+//   }
+// }
+// trait php{
+//   public function phpCoder(){
+//      return "i love java</br>";
+//   }
+// }
+
+
+// trait javaphp{
+//   use java,php;
+// }
+
+// class coderOne{
+//   use javaphp;
+// }
+
+
+
+// $obj1 = new coderOne;
+// echo $obj1->javaCoder();
+// echo $obj1->phpCoder();
+
+?>
+
+
+<?php
+
+ // Late Static Binding er maddome super class sub class er stattic propert and method gulo access kortea parea
+
+
+// class php{
+//   public static function framework(){
+//     echo "cakephp i don't know";
+//   } 
+
+//   public  function getFramework(){
+//     self::framework();
+//   }
+// }
+
+// $php = new php;
+// $php->getFramework();
+
+?>
+
+
+
+<?php
+
+class Php{
+  public static function framework(){
+   echo static::getClass();
+  }
+
+   public static function getClass(){
+    return __CLASS__;
+    }
 }
+
+
+
+class phpChild extends php{
+
+   public static function getClass(){
+    return __CLASS__;
+   }
+
 }
 
- }
+
+$php = new php;
+$php->framework();
+
+$childphp = new phpChild;
+$childphp->framework();
 
 
 
-$class = new Person;
-$class->testName();
 
 
 ?>
